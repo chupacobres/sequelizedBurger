16e5e0e0a3e2fdc10fee8091f379c93b893224be
juan uribe <juanpablouribe@mac.com>
2020-01-11 20:26:05 -0500

    added db, models, public and views

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Playa Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Crocodile Cheese-Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetarian Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marihuana Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Al Pacino Bloody Burger', true);
